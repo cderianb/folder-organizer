9b778ea4a4a655aa871a27ba2de182946faad810
cderianb <christofer.derianb@gmail.com>
2021-06-14 16:13:23 +0700

    add timer to wait watchdog event to finish

--- OrganizingEventHandler.py
+++ OrganizingEventHandler.py
@@ -1,11 +1,13 @@
 import os
 import glob
+import time
+import shutil
 from watchdog.events import FileSystemEventHandler
 
 #TODO: Add more soon!
-docs_exts = ['.pdf', '.doc', '.docx']
-img_exts = ['.jpg', 'png']
-other_exts = ['.txt']
+ignore_exts = ['.tmp', '.crdownload']
+docs_exts = ['.pdf', '.doc', '.docx', '.xls', '.xlsx']
+img_exts = ['.jpg', '.png', '.jpeg']
 compressed_exts = ['.zip']
 
 class OrganizingEventHandler(FileSystemEventHandler):
@@ -18,25 +20,10 @@ class OrganizingEventHandler(FileSystemEventHandler):
 
     def on_created(self, event):
         super().on_created(event)
+        print(f"File created - {event.src_path}")
         
-        self.event = event
-        file_path, self.file_ext = os.path.splitext(self.event.src_path)
-
-        if len(self.file_ext.strip()) == 0:
-            return # folder only
-        if len((self.event.src_path.replace(f'{self.root}\\', '')).split('\\')) > 1:
-            return
-        #kalo len nya sudah >2 artinya file yang created sudah masuk ke dalam folder, ga usah di organize lagi
-        self.file_name = file_path.split('\\')[-1]
-
-        if self.file_ext in other_exts:
-            self.move('others')
-        elif self.file_ext in docs_exts:
-            self.move('documents')
-        elif self.file_ext in img_exts:
-            self.move('images')
-        elif self.file_ext in compressed_exts:
-            self.move('compresseds')
+        if os.path.exists(event.src_path):
+            self.handle_event(event)
 
     def on_deleted(self, event):
         # return super().on_deleted(event)
@@ -45,6 +32,43 @@ class OrganizingEventHandler(FileSystemEventHandler):
         # trus kasih notif di recycle bin selama 30hari baru permanen delete
         ###
         return NotImplementedError
+    
+    def on_modified(self, event):
+        super().on_modified(event)
+        print(f"File modified - {event.src_path}")
+
+        if os.path.exists(event.src_path):
+            self.handle_event(event)
+
+    def handle_event(self, event):
+        time.sleep(1) # wait 1 second for watchdog finishing the event creation
+        
+        self.event = event
+        file_path, self.file_ext = os.path.splitext(self.event.src_path)
+        print(f'now handling {self.event.src_path}')
+        
+        if self.file_ext in ignore_exts:
+            print('ignored')
+            return
+        if len(self.file_ext.strip()) == 0:
+            print(f'folder only')
+            return # folder only
+        if len((self.event.src_path.replace(f'{self.root}\\', '')).split('\\')) > 1:
+            print(f'not handle {self.event.src_path}')
+            return
+        
+        print('working')
+        self.file_name = file_path.split('\\')[-1]
+
+        if self.file_ext in docs_exts:
+            self.move('documents')
+        elif self.file_ext in img_exts:
+            self.move('images')
+        elif self.file_ext in compressed_exts:
+            print(f'moving {self.event.src_path}')
+            self.move('compresseds')
+        else:
+            self.move('others')
 
     def move(self, type:str):
         # check for target directory existence
@@ -54,10 +78,11 @@ class OrganizingEventHandler(FileSystemEventHandler):
         #check if file with same name exist
         new_path = f'{self.root}\\{type}\\{self.file_name}{self.file_ext}'
         if os.path.exists(new_path):
-            # hitung ada berapa file dengan nama itu
-            count = len(glob.glob(f'{type}\\{self.file_name}*{self.file_ext}'))
+            # count for duplicated file
+            count = len(glob.glob(f'{self.root}\\{type}\\{self.file_name}*{self.file_ext}'))
             new_path = f'{self.root}\\{type}\\{self.file_name} ({count}) {self.file_ext}'
-        os.replace(self.event.src_path, new_path)
+        print(f'moving {self.event.src_path} to {new_path}')
+        shutil.move(self.event.src_path, new_path)
 
         #ganti jadi notif win10toast
         print(f'File moved to {type}')
--- organizer.py
+++ organizer.py
@@ -1,13 +1,14 @@
-import sys
+import os, sys, stat
 from OrganizingEventHandler import OrganizingEventHandler
 
 from watchdog.observers import Observer
 
 #Kasih parameter absolute path folder yang mau di watch 
 if __name__ == "__main__":
+    # C:\Users\Rained\Downloads
     #TODO: tambahin initialize buat awal lgsg bersihin folder nya dulu
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    
+    print(path)
     event_handler = OrganizingEventHandler()
     event_handler.root = path
 
@@ -15,8 +16,12 @@ if __name__ == "__main__":
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
+        print(f'Organizer is now watching {path}')
         while observer.is_alive():
             observer.join(1)
     except KeyboardInterrupt:
         observer.stop()
+    except FileNotFoundError as e:
+        with open('err.log', 'a') as f:
+            f.write(e)
     observer.join()
